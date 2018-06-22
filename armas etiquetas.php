58bac7263db70cf378ade3be0f078bb5e64189ef
Ignaciopetru <petruignacio@gmail.com>
2018-06-22 12:08:40 -0300

    Arreglos

--- armas etiquetas.php
+++ armas etiquetas.php
@@ -1,18 +1,10 @@
 <?php
 //conexion con base de datos
 	$conexion = mysqli_connect('localhost','root','','messuputo') or die ('No se puede conectar'.mysqli_error($conexion));
-//peticion del primer arma requerida a la base de datos
-	$pedirn = 'SELECT id, name, description FROM armas WHERE id= "'.$_POST['id'].'"';
-	$result = mysqli_query($conexion, $pedirn);
-	$row = mysqli_fetch_array($result);
-//almasenaje en la estructura data(json)
-	$data['id'] = $row['id'];
-	$data['name'] = $row['name'];
-	$data['description'] = $row['description'];
-	$array[1] = $data;
-	$i = $_POST['id'] + 1;
-//peticion de las otras 15 armas
-	while($i<=($_POST['id']+15)){
+	$ultid = $_POST['id'];
+	$i = $ultid + 1;
+//peticion de otras 5 armas
+	while($i<=($ultid+6)){
 		$pedirn = 'SELECT name, description FROM armas WHERE id="'.$i.'"';
 		$result = mysqli_query($conexion, $pedirn);
 		$row = mysqli_fetch_array($result);
